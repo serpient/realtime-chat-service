e71cdc5ede7848c8d5c2adbf6d2501b4ca7f4ca1
serpient <littlegangwolf@gmail.com>
2020-09-24 20:10:28 -0700

    Add per-room sending of messages

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ require("dotenv").config();
 const app = require("express")();
 const cors = require("cors");
 const acceptableOrigins = [
+  "*",
   "https://clever-aryabhata-d8e12c.netlify.app",
   "http://localhost:3000",
+  "https://hoppscotch.io",
 ];
 app.use(cors({ origins: acceptableOrigins }));
 const server = require("http").createServer(app);
@@ -19,42 +21,52 @@ app.get("/port", (req, res) => {
   res.send({ data: { port: process.env.PORT } });
 });
 
+const chatRooms = [
+  {
+    label: "Water Tribe",
+    name: "/waterTribe",
+  },
+  {
+    label: "Earth Kingdom",
+    name: "/earthKingdom",
+  },
+  {
+    label: "Fire Nation",
+    name: "/fireNation",
+  },
+  {
+    label: "Air Nation",
+    name: "/airNation",
+  },
+];
+
 app.get("/chat/rooms", (req, res) => {
-  const chatRooms = [
-    {
-      label: "Water Tribe",
-      name: "/waterTribe",
-    },
-    {
-      label: "Earth Kingdom",
-      name: "/earthKingdom",
-    },
-    {
-      label: "Fire Nation",
-      name: "/fireNation",
-    },
-    {
-      label: "Air Nation",
-      name: "/airNation",
-    },
-  ];
   res.send({ data: { chatRooms } });
 });
 
 chatServer.origins(acceptableOrigins);
 
+chatServer.origins((origin, callback) => {
+  if (!acceptableOrigins.includes(origin)) {
+    return callback("origin not allowed", false);
+  }
+  callback(null, true);
+});
+
 console.log(`Server is up and listening at ${process.env.PORT}`);
 
 chatServer.on("connection", (socket) => {
+  chatRooms.forEach((room) => {
+    socket.join(room.name);
+    console.log(`Creating chat room: [${room.name}]`);
+  });
   console.log("a user connected");
 
   socket.on("new_message", (data) => {
     console.log(data);
-    const { message, username, clientTimestamp } = data;
-    chatServer.sockets.emit("chat_room", {
-      message,
-      username,
-      clientTimestamp,
+    const { chatRoom } = data;
+    chatServer.in(chatRoom.name).emit(chatRoom.name, {
+      ...data,
       serverTimestamp: new Date().toISOString(),
     });
   });
